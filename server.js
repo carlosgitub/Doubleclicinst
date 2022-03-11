4356805d3e277bbef337991658ce3c3f9b7ba5fa
carlosgitub <carlos.dh08@gmail.com>
2022-03-11 18:35:11 -0500

    Cambios servidor de pruebas

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+
+// const pies = require('./server/routes/pies');
+
+const app = express();
+
+app.use(favicon(path.join(__dirname, 'dist/favicon.png')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'));
+
+console.log('Listening on port: ' + app.get('port'));
+
+module.exports = app;
